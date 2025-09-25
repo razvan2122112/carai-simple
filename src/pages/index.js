61bf95eae6a1edb85d1cf09c7c7e088044a795fb
razvan2122112 <razvanrusu2017@gmail.com>
2025-09-25 14:00:35 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
+const App = () => <div>Hello CarAI!</div>;
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
