199040db98d2d9767347d73cf908c8fa393be7fa
razvan2122112 <razvanrusu2017@gmail.com>
2025-09-25 13:59:07 +0200

    Create _document.js

---
+++ src/pages/_document.js
@@ -0,0 +1,17 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="fr">
+      <Head>
+        <meta name="description" content="CarAI - Assistant intelligent pour voiture" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
